dffe6889d8931a4ad20dada017cae376b5e4745f
Elaine Marino & Jessica Goulding <pair+elaine+jessica@commercekitchen.com>
2012-12-07 13:41:38 -0700

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KickbackV2::Application.config.secret_token = '074e625e61e22c45e9762c70c8bbed7a3ae8e7b2b7c6bcbf5093aa53cdeeb0e1ccf1c93fe9763d2f5cbf2c83a479c704171403eb4615eed73e1a9f2a806340d2'
